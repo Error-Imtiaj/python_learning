04c1342a066a7f417de74737f7270d8d75e097da
Error-Imtiaj <155739277+Error-Imtiaj@users.noreply.github.com>
2024-01-08 20:44:15 +0600

    Update start_1.py

--- beecrowd/start_1.py
+++ beecrowd/start_1.py
@@ -1,4 +1,4 @@
-a = int(input())
+## a = int(input()) ! please uncomment this to work all for loop
 
 
 ##TODO Piramid ##
@@ -43,7 +43,7 @@ for i  in range(0, a):
 
 ## ! box diamond startv printing
 
-for i in range(0, a):
+""" for i in range(0, a):
     for j in range(0, a -i):
         print(end=" ")
     for j in range(0, i):
@@ -54,4 +54,32 @@ for i in range(0 , a):
         print(end=" ")
     for j in range(0, a - i):
         print("*", end=" ")
-    print()
+    print() 
+"""
+##!  print string with loop action 
+
+
+""" import math
+import time
+
+b = input("")
+a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;'\][-!@#$%^&*()+={\|'\"}]?"
+v= " "
+k =""
+for i in b:
+    for j in a:
+        
+        if(i == v):
+            print(k+v)
+            k = k + v
+            break
+        elif(i != j):
+            print(k+j)
+            
+        elif(i == j):
+            print(k+i)
+            k = k + i
+            break
+        time.sleep(0.02)
+    
+   """ 
