ec9ad755896f968f7bbee950b0900da249440e2b
Error-Imtiaj <155739277+Error-Imtiaj@users.noreply.github.com>
2024-01-08 21:09:25 +0600

    Create digital_time.py

---
+++ beecrowd/digital_time.py
@@ -0,0 +1,16 @@
+##! DIGITAL TIME 
+from datetime import datetime
+import time
+import os
+
+
+
+
+while(1):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    time.sleep(1)
+    os.system("cls")
+    time.sleep(0.00001)
+    
